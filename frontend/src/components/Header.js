503125308f6584707da578bcbe0da2cab568b13f
tonnytech <tonnytei4@gmail.com>
2024-03-27 11:08:56 +0300

    Add product component and router setup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import {Navbar, Nav, Container} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header>
          <Navbar expand="lg" bg='dark'  variant='dark' collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/">proShop</Navbar.Brand>
+        <LinkContainer to='/'>
+        <Navbar.Brand> proShop</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'/> Cart</Nav.Link>
-            <Nav.Link href="/login"><i className='fas fa-user'/> Sign In</Nav.Link>
+          <LinkContainer to='/cart'>
+            <Nav.Link><i className='fas fa-shopping-cart'/> Cart</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+            <Nav.Link><i className='fas fa-user'/> Sign In</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
