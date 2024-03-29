76a9befadc5a2b23ec6cc67e526babc50c5e051e
tonnytech <tonnytei4@gmail.com>
2024-03-29 18:48:27 +0300

    Add coddection to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -11,7 +15,7 @@ app.get('/', (req, res)=> {
 })
 
 app.get('/api/products', (req, res)=> {
-    res.json(products);
+    res.json(products); 
 })
 
 app.get('/api/products/:id', (req, res)=> {
@@ -21,4 +25,4 @@ app.get('/api/products/:id', (req, res)=> {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
