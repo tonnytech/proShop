646984bfae1942ed4ed78cd772d0a5471357d3ac
tonnytech <tonnytei4@gmail.com>
2024-04-23 17:17:24 +0300

    Add cart screen

--- frontend/src/redux/slices/cartSlicle.js
+++ frontend/src/redux/slices/cartSlicle.js
@@ -2,11 +2,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { setLocalStorage } from '../../constants/constants';
 import { getLocalStorage } from '../../constants/constants';
+import { connections } from 'mongoose';
 
-export const cartInitialState = () => {
-    const storedCart = getLocalStorage('cartItems');
-    return storedCart ? storedCart : [];
-  };
+const cartInitialState = () => {
+  const storedCart = getLocalStorage('cartItems');
+  let returnArray = [...(storedCart || [])];
+  if (returnArray===undefined){
+    returnArray = [];
+    return returnArray;
+  }
+  return returnArray || [];
+};
 
 const initialState = {
   cartItems: cartInitialState(),
@@ -17,23 +23,29 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart(state, action) {
-     const cartItem = action.payload;
-     const existingItem = state.cartItems.find((item) => item._id === cartItem._id);
-     if(existingItem){
-        const currentItem = {
-            ...state,
-            cartItems: state.cartItems.map(x => x._id === existingItem._id ? cartItem : x)
-        }
-        setLocalStorage('cartItems', [...currentItem.cartItems]);
-        return currentItem;
-     } else {
-        const currentItem = {
-            ...state,
-            cartItems: [...state.cartItems, cartItem]
-        }
-        setLocalStorage('cartItems', [...currentItem.cartItems]);
-        return currentItem;
-     }
+      // let cartItem;
+      if(action.payload._id){
+      const cartItem = action.payload;
+       const existingItem = state.cartItems.find((item) => item._id === cartItem._id);
+       if(existingItem){
+          const currentItem = {
+              ...state,
+              cartItems: state.cartItems.map(x => x._id === existingItem._id ? cartItem : x)
+          }
+          setLocalStorage('cartItems', [...currentItem.cartItems]);
+          return currentItem;
+       } else {
+          const currentItem = {
+              ...state,
+              cartItems: [...state.cartItems, cartItem]
+          }
+          setLocalStorage('cartItems', [...currentItem.cartItems]);
+          return currentItem;
+       }
+      } else {
+        return
+      }
+    
     },
   },
 });
